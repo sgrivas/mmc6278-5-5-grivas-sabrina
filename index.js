62376b71c4ca13d44e630256f164a75051888619
Sabrina Grivas <sgrivas1@gmail.com>
2022-06-11 10:21:00 -0400

    removing and striking through list items

--- index.js
+++ index.js
@@ -13,33 +13,16 @@ form.addEventListener('submit', function(e){
         input.value='';
     }
 })
-
-ulEl.addEventListener('click', function(e){
-    if(e.target.tagName == 'BUTTON') {
-		e.target.parentElement.style.textDecoration='line-through'
-        e.target.id='done'
-	}
-})
-
-ulEl.addEventListener('click', function(e){
-    if (e.target.id='done') {
+ulEl.addEventListener('click', function(e){ 
+    if(e.target.id == '') {
+        e.target.style.textDecoration='line-through';
+        e.target.id='done';
+	} else{
         e.target.parentElement.remove();
     }
 })
 
-// form.addEventListener('submit', function(e){
-//     e.preventDefault();
-//     var input=document.querySelector('form > input');
-//     if (!(input.value == '' || input.value >= 0)){
-//         todoList.push(input.value)
 
-//         for (let index = 0; index < todoList.length; index++) {
-//             var liButtonEl=document.createElement('button');
-//             var liEl=document.createElement("li");
-//             liButtonEl.appendChild(document.createTextNode(input.value))
-//             liEl.appendChild(liButtonEl); 
-//             ulEl.appendChild(liEl);
-//             input.value='';
-//         }
-//     }
-// })
+
+
+
